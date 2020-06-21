f52bf3b8d82b5298a08da27ceeb32cb90b1920ec
Matthew J. Berger <matthewberger@nevada.unr.edu>
2020-06-21 16:32:46 -0600

    Cleanup main

--- src/main.rs
+++ src/main.rs
@@ -1,12 +1,8 @@
 mod app;
 mod renderer;
 
-use app::App;
-use log::{error, info};
+use app::{App, Error};
 
-fn main() {
-    match App::run() {
-        Ok(_) => info!("Program exited successfully."),
-        Err(error) => error!("Program failed: {:?}", error),
-    }
+fn main() -> Result<(), Error> {
+    App::run()
 }
