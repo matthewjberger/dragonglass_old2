fd2c6b96191e1416283902cd164636d5651fb2fd
Matthew J. Berger <matthewberger@nevada.unr.edu>
2020-06-21 08:51:52 -0600

    Add editorconfig. Reorganize app

--- src/app.rs
+++ src/app.rs
@@ -48,6 +48,42 @@ pub enum Error {
     },
 }
 
+pub type KeyMap = HashMap<VirtualKeyCode, ElementState>;
+
+#[derive(Default)]
+pub struct Input {
+    pub keystates: KeyMap,
+    pub mouse: Mouse,
+}
+
+pub struct Mouse {
+    pub is_left_clicked: bool,
+    pub is_right_clicked: bool,
+    pub position: glm::Vec2,
+    pub position_delta: glm::Vec2,
+    pub offset_from_center: glm::Vec2,
+    pub wheel_delta: f32,
+}
+
+impl Default for Mouse {
+    fn default() -> Self {
+        Self {
+            is_left_clicked: false,
+            is_right_clicked: false,
+            position: glm::vec2(0.0, 0.0),
+            position_delta: glm::vec2(0.0, 0.0),
+            offset_from_center: glm::vec2(0.0, 0.0),
+            wheel_delta: 0.0,
+        }
+    }
+}
+
+impl Input {
+    pub fn is_key_pressed(&self, keycode: VirtualKeyCode) -> bool {
+        self.keystates.contains_key(&keycode) && self.keystates[&keycode] == ElementState::Pressed
+    }
+}
+
 #[derive(Debug, Deserialize)]
 pub struct Settings {
     width: i64,
@@ -186,41 +222,5 @@ impl App {
             })?;
         let settings: Settings = config.try_into().context(DeserializeSettings)?;
         Ok(settings)
-    }
-}
-
-pub type KeyMap = HashMap<VirtualKeyCode, ElementState>;
-
-#[derive(Default)]
-pub struct Input {
-    pub keystates: KeyMap,
-    pub mouse: Mouse,
-}
-
-pub struct Mouse {
-    pub is_left_clicked: bool,
-    pub is_right_clicked: bool,
-    pub position: glm::Vec2,
-    pub position_delta: glm::Vec2,
-    pub offset_from_center: glm::Vec2,
-    pub wheel_delta: f32,
-}
-
-impl Default for Mouse {
-    fn default() -> Self {
-        Self {
-            is_left_clicked: false,
-            is_right_clicked: false,
-            position: glm::vec2(0.0, 0.0),
-            position_delta: glm::vec2(0.0, 0.0),
-            offset_from_center: glm::vec2(0.0, 0.0),
-            wheel_delta: 0.0,
-        }
-    }
-}
-
-impl Input {
-    pub fn is_key_pressed(&self, keycode: VirtualKeyCode) -> bool {
-        self.keystates.contains_key(&keycode) && self.keystates[&keycode] == ElementState::Pressed
     }
 }
